8314bf3d2aa6023eb2321fcfd117de9bc9698ca7
Shamel Lakin <christlife4u@gmail.com>
2020-04-03 14:25:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
+require 'pry'
 # Write your code here.
+def badge_maker(array)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  attendees.map { |person| "Hello, my name is #{person}."}
+end 
+
+def assign_rooms(attendees)
+  room_assignments = []
+  room_list = attendees.each_with_index {|name, num|  
+   "Hello, #{name}! You'll be assigned to room #{num +1}!"}
+   room_assignments << room_list
+ 
+end 
